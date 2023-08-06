4af06ba1013c846e9fb017f67e34d05fa5b47d75
leandro alves de sousa <71714626+leandro0202@users.noreply.github.com>
2023-08-06 11:43:15 -0300

    estilização de formulario

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,20 +1,25 @@
-function ProjectForm() {
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+
+function ProjectForm({btnText}) {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do Projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar Projeto"/>
-            </div>
+        <form className={styles.form}>
+            <Input
+                type='text'
+                text='Nome do Projeto'
+                placeholder='Insira o nome do projeto'
+            />
+            <Input
+                type='number'
+                text='valor do projeto'
+                name='budget'
+                placeholder='Insira o orçamento do projeto'
+            />
+            <Select name='category_id' text='Selecione uma categoria'/>
+            <SubmitButton  text={btnText}/>
 
         </form>
     )
